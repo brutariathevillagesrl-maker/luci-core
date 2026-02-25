50fead94a66bf98e01b02e67078e00aaf42ac216
brutariathevillagesrl-maker <brutariathevillagesrl@gmail.com>
2026-02-25 18:59:15 +0200

    Create config.h

---
+++ src/config.h
@@ -0,0 +1,9 @@
+#ifndef LUCI_CONFIG_H
+#define LUCI_CONFIG_H
+
+// Definiții de bază pentru LUCI Core
+#define PROJECT_NAME "LUCI"
+#define UNIT_NAME "Bees"
+#define COIN_UNIT 100000000 // 1 LUCI = 10^8 Bees
+
+#endif
