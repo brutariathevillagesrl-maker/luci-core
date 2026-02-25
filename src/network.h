4f65a2c0a7f84cac26fd092f3aed43a543526666
brutariathevillagesrl-maker <brutariathevillagesrl@gmail.com>
2026-02-25 19:48:38 +0200

    Create network.h

---
+++ src/network.h
@@ -0,0 +1,12 @@
+#ifndef LUCI_NETWORK_H
+#define LUCI_NETWORK_H
+
+#include "config.h"
+
+// Setări conexiune rețea LUCI
+#define P2P_PORT 18333       // Portul pentru mineri
+#define RPC_PORT 18332       // Portul pentru portofel
+#define NETWORK_MAGIC "LUCI" // Semnătura rețelei
+
+#endif
+
